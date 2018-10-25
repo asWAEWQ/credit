c77e12bffe57ae2e22f0b807efa8039b439568d9
asWAEWQ <43911811+asWAEWQ@users.noreply.github.com>
2018-10-25 22:15:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var cid = "503661852350349312"; // آيدي الروم هنا
+var cid = "502449717565718531"; // آيدي الروم هنا
 var nigga;
 client.on('warn', console.warn);
 client.on('error', console.error);
@@ -15,20 +15,16 @@ client.on("message", message => {
       function LoL(){
         client.channels.get(cid).send(Math.random().toString(36).substring(7));
       }
-      nigga = setInterval(LoL,305);
+      nigga = setInterval(LoL,1000);
       break;
     case "off":
       clearInterval(nigga);
       break;
   };
-  
-  client.on('message', message => {
-if(message.content.startsWith('s')) {
-if(message.author.id !== "314135031029170197") return;
-var args = message.content.split(' ').slice(1).join(' ');
-message.channel.send(args);
-}
+  if (message.content.startsWith('say')) {
+      var args = message.content.split(" ");
+      if (!args[1]) return;
+      message.channel.send(args.join(" ").slice(args[1].length));
+  }
 });
-  
-  
 client.login(process.env.BOT_TOKEN);
