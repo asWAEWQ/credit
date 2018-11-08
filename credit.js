31903eacee319d4a69a09b202ad4f5d7b9b27eb4
asWAEWQ <43911811+asWAEWQ@users.noreply.github.com>
2018-11-08 18:06:48 +0200

    Update credit.js

--- credit.js
+++ credit.js
@@ -1,3 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var cid = "502449717565718531"; // آيدي الروم هنا
+var nigga;
+client.on('warn', console.warn);
+client.on('error', console.error);
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script'));
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+client.on("message", message => {
   var prefix = "!";
   var command = message.content.split(" ")[0].slice(prefix.length);
   switch(command) {
